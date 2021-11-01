664430421c822530d382bb8a9a7601be706f47be
Sami Imran <samiimran2016@gmail.com>
2021-11-01 10:17:00 +0200

    Initial commit

---
+++ src/main/java/com/exalt/xmlfiles/XmlFilesApplication.java
@@ -0,0 +1,13 @@
+package com.exalt.xmlfiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XmlFilesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XmlFilesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exalt/xmlfiles/XmlFilesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exalt.xmlfiles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class XmlFilesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
