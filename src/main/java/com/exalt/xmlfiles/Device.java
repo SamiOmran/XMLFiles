762ddcc2cd510219ae7f0a2424852ad888a3cb0c
Sami Imran <samiimran2016@gmail.com>
2021-11-24 11:51:53 +0200

    create reference field to the parent device

--- src/main/java/com/exalt/xmlfiles/Device.java
+++ src/main/java/com/exalt/xmlfiles/Device.java
@@ -2,8 +2,6 @@ package com.exalt.xmlfiles;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import lombok.Setter;
-import org.w3c.dom.Node;
 
 import java.util.ArrayList;
 import java.util.stream.Collectors;
@@ -13,19 +11,22 @@ import java.util.stream.Collectors;
 public class Device {
     private String id;
     private String name;
-    private String parent;
+    private String parentName;
+    private Device deviceParent;
     private ArrayList<String> features;
-    private Node xmlNode;
 
-    public Device(String id, String name, String parent, ArrayList<String> features, Node xmlNode) {
+    public Device(String id, String name, String parentName, ArrayList<String> features) {
         this.id = id;
         this.name = name;
-        this.parent = parent;
+        this.parentName = parentName;
         this.features = features;
-        this.xmlNode = xmlNode;
     }
 
     public void setFeatures(ArrayList<String> features) {
         this.features = (ArrayList<String>) features.stream().distinct().collect(Collectors.toList());
+    }
+
+    public void setDeviceParent(Device deviceParent) {
+        this.deviceParent = deviceParent;
     }
 }
