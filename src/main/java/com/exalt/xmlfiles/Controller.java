38ae9abc6a8b4abdf747e2ffef6474099efa232c
Sami Imran <samiimran2016@gmail.com>
2021-12-08 14:37:04 +0200

    add needed functions for the controller and the service

--- src/main/java/com/exalt/xmlfiles/Controller.java
+++ src/main/java/com/exalt/xmlfiles/Controller.java
@@ -1,5 +1,7 @@
 package com.exalt.xmlfiles;
 
+import com.exalt.xmlfiles.model.DeviceProfileJson;
+import com.exalt.xmlfiles.model.JsonObject;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,8 +14,13 @@ public class Controller {
         this.service = service;
     }
 
-    @GetMapping(value = "/{id}", produces = {"application/json"})
-    public JsonObject getDeviceFeatures(@PathVariable String id) {
-        return service.getDeviceFeatures(id);
+//    @GetMapping(value = "/{id}", produces = {"application/json"})
+//    public JsonObject getDeviceFeatures(@PathVariable String id) {
+//        return service.getDeviceFeatures(id);
+//    }
+
+    @GetMapping(value = "/{artifactId}", produces = {"application/json"})
+    public DeviceProfileJson getDevice(@PathVariable String artifactId) {
+        return service.getDevice(artifactId);
     }
 }
--- src/main/java/com/exalt/xmlfiles/TheService.java
+++ src/main/java/com/exalt/xmlfiles/TheService.java
@@ -1,18 +1,31 @@
 package com.exalt.xmlfiles;
 
+import com.exalt.xmlfiles.model.Device;
+import com.exalt.xmlfiles.model.DeviceProfile;
+import com.exalt.xmlfiles.model.DeviceProfileJson;
+import com.exalt.xmlfiles.model.JsonObject;
 import org.springframework.stereotype.Service;
-import static com.exalt.xmlfiles.Bootstrap.allDevices;
+//import static com.exalt.xmlfiles.Bootstrap.allDevices;
+import static com.exalt.xmlfiles.Bootstrap2.allDevices;
 
 @Service
 public class TheService {
 
-    public JsonObject getDeviceFeatures(String id) {
-        for (Device tempDevice : allDevices) {
-            if (tempDevice.getId().equals(id)) {
-                return new JsonObject("Success", tempDevice);
-            }
-        }
+//    public JsonObject getDeviceFeatures(String id) {
+//        for (Device tempDevice : allDevices) {
+//            if (tempDevice.getId().equals(id)) {
+//                return new JsonObject("Success", tempDevice);
+//            }
+//        }
+//
+//        return new JsonObject("Fail", null);
+//    }
 
-        return new JsonObject("Fail", null);
+    public DeviceProfileJson getDevice(String artifactId) {
+        for (DeviceProfile deviceProfile : allDevices) {
+            if (deviceProfile.getArtifactId().equalsIgnoreCase(artifactId) || deviceProfile.getParentVersion().equalsIgnoreCase(artifactId))
+                return new DeviceProfileJson("Success", deviceProfile);
+        }
+        return new DeviceProfileJson("Fail", null);
     }
 }
